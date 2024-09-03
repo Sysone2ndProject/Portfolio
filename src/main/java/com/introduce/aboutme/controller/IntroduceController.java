80aee6b176b5431147b96f75e37ecc9e148030a9
JUNI <junehee107@naver.com>
2024-09-03 17:18:35 +0900

    chore:프로젝트 초기 구성

---
+++ src/main/java/com/introduce/aboutme/controller/IntroduceController.java
@@ -0,0 +1,17 @@
+package com.introduce.aboutme.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IntroduceController {
+  @GetMapping("/")
+  public String index(){
+    return "index";
+  }
+  @GetMapping("/members/juni")
+  public String introduceT(){
+    return "/members/juni";
+  }
+
+}
---
+++ src/test/java/com/introduce/aboutme/AboutmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.introduce.aboutme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
